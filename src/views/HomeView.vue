9cd7cb048d6932441808686639899b0dc1a7deb2
dzagorjan <dzagorjan55@gmail.com>
2026-08-13 14:39:21 +0200

    Add Tailwind CSS, test HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,25 @@
 <template>
-    <div>
-        <h1>Glucomed</h1>  
-    
-        <p>Web aplikacija za praćenje podataka povezanih s dijabetesom.</p>
+    <div class="flex min-h-screen items-center justify-center bg-slate-50">
+    <div class="flex flex-col items-center gap-6">
+      <h1 class="text-4xl font-bold text-blue-600">
+        GlucoMed
+      </h1>
+
+      <p class="text-gray-600">
+        Web aplikacija za praćenje podataka povezanih s dijabetesom.
+      </p>
+
+      <button
+        class="rounded-lg bg-blue-600 px-6 py-3 font-medium text-white hover:bg-blue-700"
+      >
+        Test
+      </button>
     </div>
+  </div>  
+
 </template>
+
+
+
+
+
